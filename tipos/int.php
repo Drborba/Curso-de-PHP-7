f70e3a5d9267987d1ba67c8aab7efba77ccefe00
Diego <diego.rborba18@gmail.com>
2022-07-23 16:10:08 -0300

    introdução aos tipos básicos em PHP

---
+++ tipos/int.php
@@ -0,0 +1,13 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+echo 11;
+echo '<br>';
+var_dump(11);
+echo '<br>';
+echo PHP_INT_MAX, '<br>';
+echo PHP_INT_MIN, '<br>';
+echo 011, '<br>'; //Base Octal
+echo 0b110100110, '<br>'; //Base Binária
+echo 0x114a8f, '<br>'; //Base Hexadecimal
+
