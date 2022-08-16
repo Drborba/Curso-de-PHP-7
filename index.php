10970b7b76855187f62430b08abed50bb978c299
Diego <diego.rborba18@gmail.com>
2022-08-16 15:35:52 -0300

    adição do while/do while

--- index.php
+++ index.php
@@ -308,6 +308,12 @@
                             Desafio Impressão
                             </a>
                         </li>
+                        <li>
+                            <a 
+                            href="exercicio.php?dir=repeticoes&file=do_while">
+                            While/Do While
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </nav>
